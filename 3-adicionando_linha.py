96fd05af6e47b03a5eeab9e143c5fec0b1dbb63c
gabrieljorda <gabrieleminhamae@hotmail.com>
2025-12-18 10:12:38 -0300

    Feat: atualização do script para adicionar compras e correção de erros de escrita

--- 3-adicionando_linha.py
+++ 3-adicionando_linha.py
@@ -4,16 +4,16 @@ import streamlit as st
 
 caminho_datasets = 'datasets'
 
-df_compras = pd.read_csv(f'{caminho_datasets}/compras.csv' , sep=';' , index_col=0)
-df_lojas = pd.read_csv(f'{caminho_datasets}/lojas.csv' , sep=';' , decimal=',' , index_col=0)
-df_produtos = pd.read_csv(f'{caminho_datasets}/produtos.csv' , sep=';' , index_col=0)   
+df_compras = pd.read_csv(f'{caminho_datasets}/compras.csv' , sep=';' , decimal=',')
+df_lojas = pd.read_csv(f'{caminho_datasets}/lojas.csv' , sep=';' , decimal=',')
+df_produtos = pd.read_csv(f'{caminho_datasets}/produtos.csv' , sep=';' , decimal=',' )   
 
 df_lojas["cidade/estado"] = df_lojas["cidade"] + '/' + df_lojas["estado"]
 lista_lojas = df_lojas['cidade/estado'].to_list()
 loja_selecionada = st.sidebar.selectbox('Selecione a loja:',lista_lojas)
 
 lista_vendedores = df_lojas.loc[df_lojas['cidade/estado'] == loja_selecionada , 'vendedores'].iloc[0]
-lista_vendedores = lista_vendedores.strip('][').replace("'" , " ").replace(']' , '').split(', ')
+lista_vendedores = lista_vendedores.strip('][').replace("'" , " ").split(', ')
 vendedor_selecionado = st.sidebar.selectbox("selecione o vendedor:", lista_vendedores)
 
 lista_produtos = df_produtos['nome'].to_list()
@@ -24,8 +24,8 @@ genero_selecionado = st.sidebar.selectbox("Genero do cliente:", ['Feminino' , 'M
 
 forma_pagamento_selecionado = st.sidebar.selectbox("Forma de Pgamento:", ['Dinheiro' , 'Cartão de Crédito' , 'Cartão de Débito' , 'Pix'])
 
-if st.sidebar.button("adicionar nova conta"):
-    lista_adicionar = [df_compras['id_compra'].max() +1 if not df_compras.empty else 1,
+if st.sidebar.button("Adicionar nova Compra"): 
+    lista_adicionar = [df_compras['id_compra'].max() + 1 if not df_compras.empty else 1,
                        loja_selecionada,
                        vendedor_selecionado,
                        produtos_selecionados,
@@ -35,8 +35,8 @@ if st.sidebar.button("adicionar nova conta"):
                        ]
     df_compras.loc[datetime.now()] = lista_adicionar
 
-    df_compras.to_csv(f'{caminho_datasets}/compras.csv' , index = False ,sep=';',descimal =',')
+    df_compras.to_csv(f'{caminho_datasets}/compras.csv',sep=';' , index=False,decimal =',')
     
-    st.succes("Compras adicionadas") 
+    st.success("Compras adicionadas") 
     
 st.dataframe(df_compras)
